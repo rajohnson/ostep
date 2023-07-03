d1a0d79b7b6cecfab9842ded61c27f290c3816fa
Rob Johnson <johnson.rob.alan@gmail.com>
2023-07-03 14:19:12 -0700

    pipe between children

--- process_api/fork.c
+++ process_api/fork.c
@@ -3,15 +3,35 @@
 #include <sys/wait.h>
 
 int main(void)
+
 {
+    int example_pipe[2];
+    pipe(example_pipe);
+
     int pid = fork();
 
     if (pid == 0)
     {
-        fclose((FILE *)STDOUT_FILENO);
-        printf("child\n");
-        int result = wait(NULL);
-        printf("result: %d\n", result);
+        printf("child 1\n");
+
+        dup2(example_pipe[1], STDOUT_FILENO);
+
+        printf("child 1 into pipe\n");
+
+        return 0;
+    }
+
+    pid = fork();
+
+    if (pid == 0)
+    {
+        printf("child 2\n");
+
+        dup2(example_pipe[0], STDIN_FILENO);
+
+        char buffer[100] = {0};
+        int readlen = read(STDIN_FILENO, buffer, 100);
+        printf("child 2 read %d bytes from pipe: %s\n", readlen, buffer);
 
         return 0;
     }
