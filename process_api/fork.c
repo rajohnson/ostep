bc205b80f183827aab46d5e1e2d377c97631b8f7
Rob Johnson <johnson.rob.alan@gmail.com>
2023-07-03 11:54:41 -0700

    fork and print a variable value

--- process_api/fork.c
+++ process_api/fork.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+#include <unistd.h>
 
 int main(void)
 {
-    printf("test\n");
+    int x = 1;
+    int pid = fork();
+
+    for (int i = 0; i < 100; i++)
+    {
+        printf("x: %d, pid: %d\n", x++, pid);
+    }
+
     return 0;
 }
